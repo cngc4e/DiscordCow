683421f92ffb54e35c28b6ba6bd20ac2151f61e8
cngc4e <cngc4e@gmail.com>
2021-12-05 15:48:35 +0800

    Revert "remote: Decode buffer from message as raw binary"
    This reverts commit 846865697ce9b40b057b89ca02683f444e54834f.

--- src/connection/remote.ts
+++ src/connection/remote.ts
@@ -128,7 +128,7 @@ class InfraConnection extends TypedEmitter<InfraConnectionEvents> {
 
         // subscribe to the receiver channel
         this.client.subscribe(this.redisChannel,
-            (message, _channel) => this.onMessagePacketReceived(Buffer.from(message, "binary")));
+            (message, _channel) => this.onMessagePacketReceived(Buffer.from(message)));
 
         this.connected = true;
     }
@@ -206,7 +206,7 @@ class InfraConnection extends TypedEmitter<InfraConnectionEvents> {
         await this.client.subscribe(
             InfraConnection.broadcastToRedisChannel(channel),
             async (message, _channel) => {
-                const bcst = await this.onBroadcastPacketReceived(channel, Buffer.from(message, "binary"));
+                const bcst = await this.onBroadcastPacketReceived(channel, Buffer.from(message));
                 subscriber.emit("message", bcst.content, bcst);
                 this.emit("broadcastReceived", bcst);
             }
